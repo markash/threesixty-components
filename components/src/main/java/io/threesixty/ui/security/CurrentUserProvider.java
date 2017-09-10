6029202acbd2b8dc63330407fdf556db2a3619a9
Mark Ashworth <mp.ashworth@gmail.com>
2017-09-10 22:21:53 +0200

    Made the return type Optional so that the supplier could indicate when the user is not available, i.e. not logged in yet.

--- components/src/main/java/io/threesixty/ui/security/CurrentUserProvider.java
+++ components/src/main/java/io/threesixty/ui/security/CurrentUserProvider.java
@@ -1,15 +1,16 @@
 package io.threesixty.ui.security;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
 /**
  * Provides the current user of the application
  * @author Mark P Ashworth (mp.ashworth@gmail.com)
  */
-public interface CurrentUserProvider<T> extends Supplier<UserPrincipal<T>> {
+public interface CurrentUserProvider<T> extends Supplier<Optional<UserPrincipal<T>>> {
     /**
      * Get the user
      * @return The user
      */
-    UserPrincipal<T> get();
+    Optional<UserPrincipal<T>> get();
 }
--- components/src/main/java/io/threesixty/ui/security/SpringSecurityCurrentUserProvider.java
+++ components/src/main/java/io/threesixty/ui/security/SpringSecurityCurrentUserProvider.java
@@ -1,6 +1,9 @@
 package io.threesixty.ui.security;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
 
 public class SpringSecurityCurrentUserProvider<T> implements CurrentUserProvider<T> {
     /**
@@ -8,7 +11,12 @@ public class SpringSecurityCurrentUserProvider<T> implements CurrentUserProvider
      * @return The user
      */
     @Override
-    public UserPrincipal<T> get() {
-        return (UserPrincipal<T>) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public Optional<UserPrincipal<T>> get() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            return Optional.ofNullable((UserPrincipal<T>) authentication.getPrincipal());
+        }
+        return Optional.empty();
     }
 }
