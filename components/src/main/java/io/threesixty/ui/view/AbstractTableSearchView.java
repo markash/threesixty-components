4198561e65e2fd9b351b4c900b19226d4aab936b
Mark Ashworth <mp.ashworth@gmail.com>
2017-10-17 08:50:12 +0200

    #18 Remove the onCreate() method from the Abstract Search View parent

--- components/src/main/java/io/threesixty/ui/view/AbstractTableSearchView.java
+++ components/src/main/java/io/threesixty/ui/view/AbstractTableSearchView.java
@@ -3,15 +3,12 @@ package io.threesixty.ui.view;
 import com.vaadin.data.Binder;
 import com.vaadin.data.provider.ListDataProvider;
 import com.vaadin.server.Responsive;
-import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.VerticalLayout;
-import io.threesixty.ui.I8n;
 import io.threesixty.ui.component.EntityGrid;
 import io.threesixty.ui.component.field.FilterField;
 import org.apache.commons.lang3.StringUtils;
-import org.vaadin.viritin.button.MButton;
 
 import java.io.Serializable;
 
@@ -20,20 +17,10 @@ import java.io.Serializable;
  * @author Mark P Ashworth
  * @version 0.2.0
  */
-@SuppressWarnings("unused")
 public abstract class AbstractTableSearchView<T extends Serializable, I extends Serializable> extends AbstractDashboardView {
 	private static final long serialVersionUID = 1L;
 
-	protected static final String BUTTON_OK = "Save";
-	protected static final String BUTTON_RESET = "Reset";
-	protected static final String BUTTON_DELETE = "Delete";
-	protected static final String BUTTON_ADD = "New";
-	protected static final String BUTTON_VIEW = "View";
-
     private EntityGrid<T> grid;
-    //private HeaderButtons headerButtons;
-    //private ExtendedFilterTextField<T> filters;
-    //private FilterField<T> filter;
 
     /**
 	 * Constructs a search view for a class that is provided by an EntityProvider and that
@@ -43,23 +30,18 @@ public abstract class AbstractTableSearchView<T extends Serializable, I extends 
 	 * @param viewCaption The caption of the view
 	 * @param dataProvider The data provider for the class
 	 * @param definition Table definition (Not used as yet)
-     * @param headerComponents The header components like buttons, search filters, etc
 	 */
 	protected AbstractTableSearchView(
 			final Class<T> beanType,
 			final String viewCaption,
 			final ListDataProvider<T> dataProvider,
-			final TableDefinition<T> definition,
-			final Component...headerComponents) {
+			final TableDefinition<T> definition) {
 		
 		super(viewCaption);
 
 		this.grid = new EntityGrid<>(beanType).withDataProvider(dataProvider).withDefinition(definition);
         getToolbar().setFilter(new FilterField<>(dataProvider, definition));
-        getToolbar().addAction(new MButton(I8n.Button.NEW, this::onCreate));
 	}
-	
-	public abstract void onCreate(ClickEvent event);
 
 	/**
 	 * Builds the navigation state for the entity view path, i.e. !#entityViewName/id
